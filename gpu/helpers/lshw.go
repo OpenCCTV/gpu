42c1b6377fc1a93abf966c8c4bfe80f94675e03c
Shuge Lee <shuge.lee@gmail.com>
2019-06-19 14:43:31 +0800

    Guess vendor via collector binary if issue lshw failed

--- gpu/helpers/lshw.go
+++ gpu/helpers/lshw.go
@@ -2,43 +2,66 @@ package helpers
 
 import (
 	"errors"
+	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 )
 
+const (
+	pathBinNvidia = "/usr/bin/nvidia-smi"
+	pathBinIntel  = "/usr/bin/intel_gpu_top"
+)
+
 // Get display(GPU) venrder info by `lshw`.
 func GuessGPUVendors() (vendors []string, err error) {
-	var errmsg string
-	out, err := exec.Command("bash", "-c", `lshw -c display | grep vendor`).Output()
+	_cmd := `lshw -c display | grep vendor`
+	out, err := exec.Command("bash", "-c", _cmd).Output()
 	if err != nil {
-		errmsg = "get lshw info failed"
-		log.Println(errmsg, err)
-		return vendors, errors.New(errmsg)
-	}
+		log.Println(fmt.Sprintf("[error] issue command failed -%s-", _cmd))
 
-	lines := strings.Split(string(out), "\n")
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
-		}
-		columns := strings.Split(line, ":")
-		if len(columns) != 2 {
-			log.Println("parse vendor info failed", line, columns)
-			continue
+		_, err := os.Stat(pathBinNvidia)
+		if err == nil {
+			vendors = append(vendors, "nvidia")
 		}
 
-		vendor := strings.TrimSpace(columns[1])
-		vendor = strings.ToLower(vendor)
-		if strings.Index(vendor, "intel") != -1 {
-			vendor = "intel"
-		} else if strings.Index(vendor, "nvidia") != -1 {
-			vendor = "nvidia"
+		_, err = os.Stat(pathBinIntel)
+		if err == nil {
+			vendors = append(vendors, "intel")
 		}
 
-		if vendor != "" {
-			vendors = append(vendors, vendor)
+		if len(vendors) > 0 {
+			return vendors, nil
+		}
+
+		err = errors.New("issue lshw failed and both nvidia and intel collector tool not found")
+		return vendors, err
+
+	} else {
+		lines := strings.Split(string(out), "\n")
+		for _, line := range lines {
+			line = strings.TrimSpace(line)
+			if line == "" {
+				continue
+			}
+			columns := strings.Split(line, ":")
+			if len(columns) != 2 {
+				log.Println("parse vendor info failed", line, columns)
+				continue
+			}
+
+			vendor := strings.TrimSpace(columns[1])
+			vendor = strings.ToLower(vendor)
+			if strings.Index(vendor, "intel") != -1 {
+				vendor = "intel"
+			} else if strings.Index(vendor, "nvidia") != -1 {
+				vendor = "nvidia"
+			}
+
+			if vendor != "" {
+				vendors = append(vendors, vendor)
+			}
 		}
 	}
 
